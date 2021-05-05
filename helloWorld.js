f3a880ae069ee83fdac4cc61e5cd019bce9624fe
noobmasterdihan <dihanahmed205@gmail.com>
2021-05-06 00:14:48 +0600

    Completed global variables and Local modules

--- helloWorld.js
+++ helloWorld.js
@@ -2,7 +2,7 @@
 // exports should be done after function
 
 module.exports.Dihan=()=>{
-    console.log("this is a restricted area!");
+    console.log("this is a restricted area! and its a dead space!");
 }
 
 module.exports.name="Isaak klarke";
